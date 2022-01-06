309931a15829a526f6a5600b1330cf6dc4d250b5
arnoj <john.arnold@pega.com>
2022-01-06 16:02:32 -0500

    Initial fixes

--- src/helpers/versionHelpers.ts
+++ src/helpers/versionHelpers.ts
@@ -7,37 +7,18 @@
 
 declare const PCore;
 
-export const sdkVersion = "8.6";
-
-export function is86(): boolean {
-  let bRet = false;
-  const theVer = PCore.getPCoreVersion();
-
-  if (theVer.includes("1.0") || theVer.includes("8.6")) {
-    bRet = true;
-  }
-
-  return bRet;
-}
-
-
-export function is87(): boolean {
-  let bRet = false;
-  const theVer = PCore.getPCoreVersion();
-
-  if (theVer.includes("8.7")) {
-    bRet = true;
-  }
-
-  return bRet;
-}
+export const sdkVersion = "8.7";
 
 export function compareSdkPCoreVersions() {
-  if (is86() && (sdkVersion.includes("8.6"))) {
+
+  const theConstellationVersion = PCore.getPCoreVersion();
+
+  if (theConstellationVersion.includes(sdkVersion)) {
     // eslint-disable-next-line no-console
     console.log(`sdkVersion: ${sdkVersion} matches PCore version: ${PCore.getPCoreVersion()}`);
   } else {
     // eslint-disable-next-line no-console
     console.error(`sdkVersion: ${sdkVersion} does NOT match PCore version: ${PCore.getPCoreVersion()}`);
   }
+
 }

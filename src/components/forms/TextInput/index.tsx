ad43c0ecf909ad2aa569911a63b55b32626bf1ec
tumms2021389 <sivaramakrishna.tumma@in.pega.com>
2022-10-12 00:06:23 +0530

    Fix: Restricting user to enter text atmost max length

--- src/components/forms/TextInput/index.tsx
+++ src/components/forms/TextInput/index.tsx
@@ -1,10 +1,24 @@
-import React from "react";
-import { TextField } from "@material-ui/core";
+import React from 'react';
+import { TextField } from '@material-ui/core';
 
 export default function TextInput(props) {
-  const {label, required, disabled, value='', validatemessage, status, onChange, onBlur, readOnly, testId} = props;
+  const {
+    label,
+    required,
+    disabled,
+    value = '',
+    validatemessage,
+    status,
+    onChange,
+    onBlur,
+    readOnly,
+    testId,
+    fieldMetadata
+  } = props;
 
-  let readOnlyProp = {};  // Note: empty if NOT ReadOnly
+  const maxLength = fieldMetadata?.maxLength;
+
+  let readOnlyProp = {}; // Note: empty if NOT ReadOnly
 
   if (readOnly) {
     readOnlyProp = { readOnly: true };
@@ -13,24 +27,24 @@ export default function TextInput(props) {
   let testProp = {};
 
   testProp = {
-     "data-test-id": testId
+    'data-test-id': testId
   };
 
   return (
-      <TextField
-        fullWidth
-        variant={ readOnly ? "standard" : "outlined"}
-        helperText={validatemessage}
-        placeholder=""
-        size="small"
-        required={required}
-        disabled={disabled}
-        onChange={onChange}
-        onBlur={onBlur}
-        error={status === "error"}
-        label={label}
-        value={value}
-        InputProps={ { ...readOnlyProp, inputProps: { ...testProp }} }
-      />
-    )
+    <TextField
+      fullWidth
+      variant={readOnly ? 'standard' : 'outlined'}
+      helperText={validatemessage}
+      placeholder=''
+      size='small'
+      required={required}
+      disabled={disabled}
+      onChange={onChange}
+      onBlur={onBlur}
+      error={status === 'error'}
+      label={label}
+      value={value}
+      InputProps={{ ...readOnlyProp, inputProps: { maxLength, ...testProp } }}
+    />
+  );
 }

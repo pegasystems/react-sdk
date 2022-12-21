3efd8b4aa7d0072b11196ba75dc992541893f132
Vishal Sharma <vishalshrm539@gmail.com>
2022-12-21 19:22:24 +0530

    Fixed Case Reference tests failing occasionally and few typos (#212)

--- tests/e2e/Digv2/CaseReference.spec.js
+++ tests/e2e/Digv2/CaseReference.spec.js
@@ -10,7 +10,7 @@ test.beforeEach(async ({ page }) => {
 });
 
 test.describe('E2E test', () => {
-  test('should login, create case and run different test cases for Data Reference', async ({
+  test('should login, create case and run different test cases for Case Reference', async ({
     page
   }) => {
     await common.Login(
@@ -132,7 +132,8 @@ test.describe('E2E test', () => {
 
     await page.locator('button:has-text("Next")').click();
 
-    await expect(page.locator(`td >> text="${caseID[0]}"`)).toBeVisible();
+    const assignment = page.locator('div[id="Assignment"]');
+    await expect(assignment.locator(`div >> span >> text="${caseID[0]}"`)).toBeVisible();
 
     await page.locator('button:has-text("Previous")').click();
 

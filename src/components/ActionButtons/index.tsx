600dfc56baeb583a61db9c39872577126c158182
Akhila <akhila.mineendsouza@hmrc.gov.uk>
2023-02-27 16:36:35 +0000

    Fixed lint issues

--- src/components/ActionButtons/index.tsx
+++ src/components/ActionButtons/index.tsx
@@ -1,22 +1,10 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { makeStyles } from '@material-ui/core/styles';
-
 import Button from '@material-ui/core/Button';
 import { Grid } from "@material-ui/core";
 
-
-
-const useStyles = makeStyles((/* theme */) => ({
-  button: {
-    padding: "0px 5px",
-  }
-}));
-
-
 export default function ActionButtons(props) {
   const { arMainButtons, arSecondaryButtons, onButtonPress } = props;
-  const classes = useStyles();
 
   function _onButtonPress(sAction: string, sButtonType: string) {
 

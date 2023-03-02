8d163e22f3f1ffee8d826e3d6438f50bbdd01647
peter.maloney1@hmrc.gov.uk <U.7293773@columbus.local>
2023-03-02 10:58:56 +0000

    add aria-describedby attribute to select component

--- src/components/BaseComponents/Select/Select.tsx
+++ src/components/BaseComponents/Select/Select.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import FormGroup from '../FormGroup/FormGroup';
+import FormGroup, {makeHintId, makeErrorId} from '../FormGroup/FormGroup';
 
 export default function Select(props){
 
-  const {name, onChange, value, children} = props;
+  const {name, onChange, value, children, errorText, hintText} = props;
+
+  const describedbyIds = `${errorText?makeErrorId(name):""} ${hintText?makeHintId(name):""}`.trim();
 
   return(
     <FormGroup {...props}>
-        <select className="govuk-select" id={name} name={name} onChange={onChange} value={value}>
+        <select className="govuk-select" id={name} name={name} onChange={onChange} value={value} aria-describedby={describedbyIds}>
           {children}
         </select>
     </FormGroup>

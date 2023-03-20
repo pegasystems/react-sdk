9ef62cbfba2e4e0a4844bcac6736bfd138b917c5
peter-mal <122813299+peter-mal@users.noreply.github.com>
2023-03-20 17:55:42 +0000

    Proposed fix for label heading bugs (#25)
    * Potential solution for page h1 handling, sets label of child of default view with one editable field to be the stage name
    
    * removed debugging console loh
    
    * Resolves issue in edgecase where there is one editable field alongside read only fields
    
    ---------
    
    Co-authored-by: peter.maloney1@hmrc.gov.uk <U.7293773@columbus.local>

--- src/components/templates/DefaultForm/index.tsx
+++ src/components/templates/DefaultForm/index.tsx
@@ -1,20 +1,27 @@
 import React, { createElement } from "react";
 
 import createPConnectComponent from '../../../bridge/react_pconnect';
+import isOnlyOneField from '../../../helpers/hooks/QuestionDisplayHooks';
 
 import './DefaultForm.css';
 
 export default function DefaultForm(props) {
   const { getPConnect } = props;
 
+  const onlyOneField = isOnlyOneField();
+
   // repoint the children because they are in a region and we need to not render the region
   // to take the children and create components for them, put in an array and pass as the
   // defaultForm kids
   const arChildren = getPConnect().getChildren()[0].getPConnect().getChildren();
 
-  const dfChildren = arChildren.map((kid, idx) =>
-    createElement(createPConnectComponent(), { ...kid, key: idx }) // eslint-disable-line react/no-array-index-key
-  );
+  const dfChildren = arChildren.map((kid, idx) =>{
+    const childPConnect = kid.getPConnect();
+    if(onlyOneField && childPConnect.getConfigProps().readOnly !== true){
+      childPConnect.setInheritedProp('label', getPConnect().getDataObject().caseInfo.assignments[0].name);
+    }
+    return createElement(createPConnectComponent(), { ...kid, key: idx }) // eslint-disable-line react/no-array-index-key
+  });
 
   return <>{dfChildren}</>;
 }

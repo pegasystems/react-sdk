ff6f1c917664f61251e1581d2f908b9a8667464f
monalisasarkar123 <126481002+monalisasarkar123@users.noreply.github.com>
2023-03-28 15:25:06 +0100

    confirmation Page Accessibility Issue Fix (#34)
    * confirmation Page Accessibility Issue Fix
    
    * Changes in the fix for Child Benefits Claim

--- src/applications/ChildBenefitsClaim/ConfirmationPage.tsx
+++ src/applications/ChildBenefitsClaim/ConfirmationPage.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 
 const ConfirmationPage = () => {
   // const getPConnect = props;
@@ -7,23 +7,17 @@ const ConfirmationPage = () => {
   // const caseId = PCore.caseId
 
   return (
-    <div className='govuk-width-container' id='content'>
-      <main className='govuk-main-wrapper govuk-main-wrapper--l' id='main-content' role='main'>
-        <div className='govuk-grid-row'>
-          <div className='govuk-grid-column-two-thirds'>
-            <div className='govuk-panel govuk-panel--confirmation'>
-              <h1 className='govuk-panel__title'>Application for Child Benefit complete</h1>
-            </div>
-            <h2 className='govuk-heading-m'>What happens next</h2>
-            <p className='govuk-body'>We&apos;ve sent your application to Child Benefit Service.</p>
-            <p className='govuk-body'>
-              We&apos;ll tell you in 14 calendar days if your application has been successful.
-            </p>
-          </div>
-        </div>
-      </main>
+    <div className='govuk-grid-column-two-thirds'>
+      <div className='govuk-panel govuk-panel--confirmation'>
+        <h1 className='govuk-panel__title'>Application for Child Benefit complete</h1>
+      </div>
+      <h2 className='govuk-heading-m'>What happens next</h2>
+      <p className='govuk-body'>We&apos;ve sent your application to Child Benefit Service.</p>
+      <p className='govuk-body'>
+        We&apos;ll tell you in 14 calendar days if your application has been successful.
+      </p>
     </div>
   );
-}
+};
 
 export default ConfirmationPage;

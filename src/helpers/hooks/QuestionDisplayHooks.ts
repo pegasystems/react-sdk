64c83de379fefd65ce7acbc875ff478614bd663a
peter-mal <122813299+peter-mal@users.noreply.github.com>
2023-04-05 12:46:58 +0100

    Bug 5925 - Fixes issue where in put on check answers screen takes the page title (#40)
    * Initial changes to hook
    
    * Resolved issue where there is a single editable input but it is not the only item on the page
    
    ---------
    
    Co-authored-by: peter.maloney1@hmrc.gov.uk <U.7293773@columbus.local>

--- src/helpers/hooks/QuestionDisplayHooks.ts
+++ src/helpers/hooks/QuestionDisplayHooks.ts
@@ -9,10 +9,12 @@ declare const PCore;
 
 
 export default function useIsOnlyField(effectTrigger = null){
-  const [isOnlyField, setisOnlyField] = useState(PCore.getFormUtils().getEditableFields("root/primary_1/workarea_1").length === 1);
+  const [isOnlyField, setisOnlyField] = useState(PCore.getFormUtils().getEditableFields("root/primary_1/workarea_1").length === 1
+  && PCore.getFormUtils().getEditableFields("root/primary_1/workarea_1")[0].fieldC11nEnv.getComponent().props.displayOrder.split('-')[0] !== '0');
 
   useEffect ( () => {
-    setisOnlyField(PCore.getFormUtils().getEditableFields("root/primary_1/workarea_1").length === 1);
+    setisOnlyField(PCore.getFormUtils().getEditableFields("root/primary_1/workarea_1").length === 1
+    && PCore.getFormUtils().getEditableFields("root/primary_1/workarea_1")[0].fieldC11nEnv.getComponent().props.displayOrder.split('-')[0] === '0');
   }, [effectTrigger])
 
   return isOnlyField;

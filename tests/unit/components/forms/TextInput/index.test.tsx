e23a172b6329734b2ad118e82dab19a2eabc3b9e
vishal sharma <vishalshrm539@gmail.com>
2023-04-19 18:35:55 +0530

    Added tests for TextInput, added a couple of tests in the unit test, and updated the structure of the tests folder

--- tests/unit/components/forms/TextInput/index.test.tsx
+++ tests/unit/components/forms/TextInput/index.test.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import TextInput from '../../../../../src/components/forms/TextInput';
 
+const onBlur = jest.fn();
+const onChange = jest.fn();
 const getDefaultProps = (): any => {
   return {
     required: true,
     testId: 'textInputTestId',
     label: 'TextInput',
-    displayMode: false
+    displayMode: false,
+    onChange,
+    onBlur
   };
 };
 
@@ -58,5 +62,27 @@ describe('Test Text Input component', () => {
     const { getByText } = render(<TextInput {...props} />);
     const readOnlySpan = getByText('Hi there!');
     expect(readOnlySpan).toBeVisible();
+  });
+
+  test('TextInput Component invoke handlers for blur and change events', () => {
+    const props = getDefaultProps();
+    const TextInputComponent = render(<TextInput {...props} />);
+    fireEvent.change(TextInputComponent.getByTestId('textInputTestId'), {
+      target: { value: 'a' }
+    });
+    fireEvent.blur(TextInputComponent.getByTestId('textInputTestId'));
+    expect(onChange).toHaveBeenCalled();
+    expect(onBlur).toHaveBeenCalled();
+  });
+
+  test('TextInput Component should not invoke on blur handler for read only fields', () => {
+    const props = getDefaultProps();
+    props.readOnly = true;
+    const TextInputComponent = render(<TextInput {...props} />);
+    fireEvent.change(TextInputComponent.getByTestId('textInputTestId'), {
+      target: { value: 'a' }
+    });
+    fireEvent.blur(TextInputComponent.getByTestId('textInputTestId'));
+    expect(onBlur).toHaveBeenCalled();
   });
 });

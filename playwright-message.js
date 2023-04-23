a35c7dc102f36b425fcb611aac74c1a4ac724a3f
arnoj <john.arnold@pega.com>
2023-04-22 20:23:52 -0400

    Clearing out main to prepare for change

--- playwright-message.js
+++
@@ -1,1 +0,0 @@
-console.log('\x1b[33m%s\x1b[0m','Running in headless mode, use `npm run test:headed` for running in headed mode.');
--- tests/setUpTests.js
+++
@@ -1,4 +0,0 @@
-import '@testing-library/jest-dom/extend-expect';
-import { configure } from '@testing-library/react';
-
-configure({ testIdAttribute: 'data-test-id' });

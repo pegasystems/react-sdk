b0b760726c90c1112009a4ce643536ee48f60b58
manasa <manasa.mashetty@in.pega.com>
2023-05-02 21:15:54 +0530

    decimal field issue fixed

--- src/components/forms/Decimal/index.tsx
+++ src/components/forms/Decimal/index.tsx
@@ -1,39 +1,78 @@
-import React from 'react';
-import { TextField } from '@material-ui/core';
-import TextInput from '../TextInput';
+import React, { useState, useEffect } from 'react';
+import CurrencyTextField from '@unicef/material-ui-currency-textfield';
 import FieldValueList from '../../designSystemExtensions/FieldValueList';
+import { getCurrencyCharacters, getCurrencyOptions } from '../Currency/currency-utils';
+import handleEvent from '../../../helpers/event-utils';
+import { format } from "../../../helpers/formatters";
+
+// Using control from: https://github.com/unicef/material-ui-currency-textfield
 
 export default function Decimal(props) {
   const {
+    getPConnect,
     label,
     required,
     disabled,
     value = '',
     validatemessage,
     status,
-    onChange,
-    onBlur,
+    /* onChange, onBlur, */
     readOnly,
     helperText,
     displayMode,
-    hideLabel
+    hideLabel,
+    currencyISOCode,
+    decimalPrecision = 2,
+    showGroupSeparators
   } = props;
+
+  const pConn = getPConnect();
+  const actions = pConn.getActionsApi();
+  const propName = pConn.getStateProps().value;
   const helperTextToDisplay = validatemessage || helperText;
 
+  const [decValue, setDecimalvalue] = useState();
+  const [theCurrDec, setCurrDec] = useState(".");
+  const [theCurrSep, setCurrSep] = useState(",");
+
+  useEffect(() => {
+    const theSymbols = getCurrencyCharacters(currencyISOCode);
+    setCurrDec(theSymbols.theDecimalIndicator);
+    setCurrSep(theSymbols.theDigitGroupSeparator);
+  }, [currencyISOCode]);
+
+  useEffect(() => {
+    setDecimalvalue(value.toString());
+  }, [value]);
+
+  const theCurrencyOptions = getCurrencyOptions(currencyISOCode);
+  const formattedValue = format(value, pConn.getComponentName().toLowerCase(), theCurrencyOptions);
+
   if (displayMode === 'LABELS_LEFT') {
-    return <FieldValueList name={hideLabel ? '' : label} value={value} />;
+    return <FieldValueList name={hideLabel ? '' : label} value={formattedValue} />;
   }
 
   if (displayMode === 'STACKED_LARGE_VAL') {
-    return <FieldValueList name={hideLabel ? '' : label} value={value} variant='stacked' />;
+    return <FieldValueList name={hideLabel ? '' : label} value={formattedValue} variant='stacked' />;
   }
 
+  let readOnlyProp = {}; // Note: empty if NOT ReadOnly
+
   if (readOnly) {
-    return <TextInput {...props} />;
+    readOnlyProp = { readOnly: true };
+  }
+
+  function decimalOnChange(event) {
+    // update internal value
+    setDecimalvalue(event?.target?.value);
+  }
+
+  function decimalOnBlur(event, inValue) {
+    handleEvent(actions, 'changeNblur', propName, inValue !== '' ? Number(inValue) : inValue);
   }
 
   return (
-    <TextField
+    <CurrencyTextField
       fullWidth
       variant={readOnly ? 'standard' : 'outlined'}
       helperText={helperTextToDisplay}
@@ -41,13 +80,19 @@ export default function Decimal(props) {
       size='small'
       required={required}
       disabled={disabled}
-      onChange={onChange}
-      onBlur={!readOnly ? onBlur : undefined}
+      onChange={decimalOnChange}
+      onBlur={!readOnly ? decimalOnBlur : undefined}
       error={status === 'error'}
       label={label}
-      value={value}
+      value={decValue}
       type='text'
-      inputProps={{ inputMode: 'decimal', pattern: '[0-9]*' }}
+      outputFormat='number'
+      textAlign='left'
+      InputProps={{ ...readOnlyProp, inputProps: { value: decValue } }}
+      currencySymbol=''
+      decimalCharacter={theCurrDec}
+      digitGroupSeparator={showGroupSeparators ? theCurrSep : ''}
+      decimalPlaces={decimalPrecision}
     />
   );
 }

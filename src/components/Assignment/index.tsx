2ce619630eabb16832ce9a34b5394aa023bb50b7
monalisasarkar123 <126481002+monalisasarkar123@users.noreply.github.com>
2023-05-31 10:23:44 +0100

    BUG-6220 fix (#61)
    * BUG-6220 fix
    
    * Backlink UX changes added to this PR
    
    * resolving PR comments

--- src/components/Assignment/index.tsx
+++ src/components/Assignment/index.tsx
@@ -164,7 +164,7 @@ export default function Assignment(props) {
   }
 
   function scrollToTop(){
-    const position = document.querySelector('h1')?.offsetTop || 0;
+    const position = document.getElementById('#main-content')?.offsetTop || 0;
     document.body.scrollTop = position;
     document.documentElement.scrollTop = position;
   }
--- src/components/BaseComponents/BackLink/BackLink.tsx
+++ src/components/BaseComponents/BackLink/BackLink.tsx
@@ -5,7 +5,7 @@ export default function BackLink(props) {
   const { onClick, children, attributes } = props;
 
   return (
-    <a href='#' onClick={onClick} {...attributes} className='govuk-back-link govuk-!-margin-top-0 govuk-!-margin-bottom-5'>
+    <a href='#' onClick={onClick} {...attributes} className='govuk-back-link govuk-!-margin-top-0 govuk-!-margin-bottom-8'>
      Back {children}
     </a>
   );

d62aa36dc98c69b6350eec4bc31272b527d9bda0
tumms2021389 <sivaramakrishna.tumma@in.pega.com>
2023-06-14 20:07:50 +0530

    fix: lint error

--- src/helpers/common-utils.ts
+++ src/helpers/common-utils.ts
@@ -1,18 +1,17 @@
-// eslint-disable-next-line import/prefer-default-export
 export function isEmptyObject(obj: Object): Boolean {
   return Object.keys(obj).length === 0;
 }
 
-export function processQueryParams(){
+export function processQueryParams() {
   const queryString = window.location.search;
   const urlParams = new URLSearchParams(queryString);
-  const portal = urlParams.get("portal");
-  const locale = urlParams.get("locale");
+  const portal = urlParams.get('portal');
+  const locale = urlParams.get('locale');
 
   if (portal) {
-    sessionStorage.setItem("rsdk_portalName", portal);
+    sessionStorage.setItem('rsdk_portalName', portal);
   }
-  if(locale){
+  if (locale) {
     sessionStorage.setItem('rsdk_locale', locale);
   }
 }

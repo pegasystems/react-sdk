000c44ba20ffab707fbade01d16f6cb5fbf39f6d
tumms2021389 <97188066+tumms2021389@users.noreply.github.com>
2023-06-16 20:41:43 +0530

    Re-rendering the child components based on visibility when store changes (#351)
    * Re-rendering the child components based on visibility when store changes

---
+++ src/utils/state-utils.tsx
@@ -0,0 +1,48 @@
+import React, { useContext } from 'react';
+import { connect, Provider, shallowEqual } from 'react-redux';
+import ReactReduxContext from '../bridge/Context/StoreContext';
+
+declare const PCore: any;
+
+const connectToState = (mapStateToProps = () => {}) => {
+  return (Component: any) => {
+    const ConnectedComponent = connect(mapStateToProps, null, null, {
+      areStatePropsEqual: (next, prev: any) => {
+        // Compare visibility
+        const prevWasVisible = prev.visibility !== false;
+        if (next.visibility !== undefined && next.visibility !== prevWasVisible) {
+          return false;
+        }
+        // Compare start props
+        const c11nEnv = next.getPConnect();
+        const allStateProps = c11nEnv.getStateProps();
+        for (const key in allStateProps) {
+          if (
+            !shallowEqual(next[key], prev[key]) ||
+            (next.routingInfo && !PCore.isDeepEqual(next.routingInfo, prev.routingInfo))
+          ) {
+            return false;
+          }
+        }
+        /* TODO For some rawConfig we are not getting routingInfo under allStateProps */
+        return !(
+          'routingInfo' in next &&
+          (!shallowEqual(next.routingInfo, prev.routingInfo) ||
+            !PCore.isDeepEqual(next.routingInfo, prev.routingInfo))
+        );
+      }
+    })(Component);
+
+    return (ownProps = {}) => {
+      const { store } = useContext(ReactReduxContext);
+
+      return (
+        <Provider store={store}>
+          <ConnectedComponent {...ownProps} />
+        </Provider>
+      );
+    };
+  };
+};
+
+export default connectToState;

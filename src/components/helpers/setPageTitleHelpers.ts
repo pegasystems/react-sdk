906feba90d3522d2064bac3f6170aab1c459fcfd
peter-mal <122813299+peter-mal@users.noreply.github.com>
2023-08-18 12:15:07 +0100

    Add bug-6389 to upgrade branch + add translation handling (#114)
    * Add bug-6389 to upgrade branch + add translation handling
    
    * Translation added for the service name in page title
    
    ---------
    
    Co-authored-by: peter.maloney1@hmrc.gov.uk <U.7293773@columbus.local>
    Co-authored-by: Akhila <akhila.mineendsouza@hmrc.gov.uk>

--- src/components/helpers/setPageTitleHelpers.ts
+++ src/components/helpers/setPageTitleHelpers.ts
@@ -1,4 +1,5 @@
 
+import i18n from "i18next";
 /*
   setPageTile(pageHeading:string, serviceName:string)
   Sets page title to in following pattern
@@ -6,10 +7,25 @@
 */
 export default function setPageTitle(){
 
-  const pageHeading = document.getElementsByTagName('h1')[0]?.innerText
+    // In case where there may be multiple H1s on page (e.g. breifly may have pega part of page and confirmation screen shown together)
+    // set up an interval to keep checking until we have only one H1, and then set the page title with the remaining H1
+    // This assumes we follow the best practice to only display one h1 on the page, and simply works around the 'gap' when our main
+    // page updates to hide/show components.
+    if(document.getElementsByTagName('h1').length > 1){
+      const setPageTitleInterval = setInterval(
+        () => {
+          if(document.getElementsByTagName('h1').length === 1){
+            clearInterval(setPageTitleInterval);
+            setPageTitle();
+          }
+        }, 500)
+    }
 
-  // Scope to fetch serviceName dynamically from here
-  const serviceName = 'Child benefit claim';
+    const pageHeading = i18n.t(document.getElementsByTagName('h1')[0]?.innerText);
+
+    // Scope to fetch serviceName dynamically from here
+    // TODO fetch serviceName dynamically
+    const serviceName = i18n.t("CLAIM_CHILD_BENEFIT");
 
   if(pageHeading){
   document.title = `${pageHeading} - ${serviceName} - GOV.UK`

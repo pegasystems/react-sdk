ec74247f233973a8ee16da2a1241c5fc587945d2
Stefan (CDIO) Sokolowski <U.7866928@columbus.local>
2023-08-25 10:23:26 +0100

    feat US-10731: refactoring functions

--- src/components/Utils/cookie.ts
+++ src/components/Utils/cookie.ts
@@ -4,15 +4,28 @@ const COOKIE_PEGAODXDI = 'pegaodxdi';
 const HASH = 'Pn5AYySRgIZP9MYbh27Ow';
 const date400daysLater = () => new Date(new Date().getTime() + (8.64e+7 * 400)).toUTCString();
 
-export const setCookie = (cname: string) => {
-    // requirements for future cookies unknown, hence the switch statement
-    switch (cname) {
-        case COOKIE_PEGAODXDI:
-            const cookieValue = `${cname}#${uuid()}#${new Date().getTime()}_${HASH}`;
-            document.cookie = `${cname}=${cookieValue};expires=${date400daysLater()};path=/`;
-        default:
-            return null;
-    };
+export const setCookie = (cname: string): void => {
+    // requirements for future cookies unknown, hence the check
+    if (cname === COOKIE_PEGAODXDI) {
+        const cookieValue = `${cname}#${uuid()}#${new Date().getTime()}_${HASH}`;
+        document.cookie = `${cname}=${cookieValue};expires=${date400daysLater()};path=/`;
+    }
+};
+
+export const getCookie = (cname: string) => {
+    const name = `${cname}=`;
+    const decodedCookie = decodeURIComponent(document.cookie);
+    const ca = decodedCookie.split(';');
+    for (let i = 0; i < ca.length; i += 1) {
+        let c = ca[i];
+        while (c.startsWith(' ')) {
+            c = c.substring(1);
+        }
+        if (c.startsWith(name)) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
 };
 
 export const updateCookieValue = (cname: string, value: string = '') => {
@@ -27,28 +40,14 @@ export const updateCookieAttribute = (cname: string, atrName: string, atrVal: st
     };
 };
 
-export const getCookie = (cname: string) => {
-    let name = cname + "=";
-    let decodedCookie = decodeURIComponent(document.cookie);
-    let ca = decodedCookie.split(';');
-    for (let i = 0; i < ca.length; i++) {
-        let c = ca[i];
-        while (c.charAt(0) === ' ') {
-            c = c.substring(1);
-        }
-        if (c.indexOf(name) === 0) {
-            return c.substring(name.length, c.length);
-        }
-    }
-    return "";
-};
-
 export const checkCookie = (cname: string) => getCookie(cname);
 
 export const getDeviceId = () => {
-    checkCookie(COOKIE_PEGAODXDI)
-        ? updateCookieAttribute(COOKIE_PEGAODXDI, 'expires', date400daysLater())
-        : setCookie(COOKIE_PEGAODXDI)
+    if (checkCookie(COOKIE_PEGAODXDI)) {
+        updateCookieAttribute(COOKIE_PEGAODXDI, 'expires', date400daysLater())
+    } else {
+        setCookie(COOKIE_PEGAODXDI)
+    }
     return getCookie(COOKIE_PEGAODXDI);
 };
 

403d124fd6483266103da1f739ef33cd95249141
manasa <manasa.mashetty@in.pega.com>
2023-10-13 16:04:58 +0530

    lint error fixed

--- src/components/templates/ListView/index.tsx
+++ src/components/templates/ListView/index.tsx
@@ -6,7 +6,6 @@
 import React, { useState, useEffect, useRef } from 'react';
 import PropTypes from 'prop-types';
 import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
-import { Utils } from '../../../helpers/utils';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';

6dcc6fce81ff9e9ef27ebccf85e9127b30435663
J_Arnold <john.arnold@pega.com>
2023-11-08 11:28:54 -0500

    8.8.20-Storybook-Updates (#397)

--- .storybook/main.js
+++ .storybook/main.js
@@ -12,6 +12,14 @@ module.exports = {
   webpackFinal: (config) => {
     config.resolve.alias['@pega/react-sdk-components/lib/bridge/react_pconnect'] = path.resolve(__dirname, '../__mocks__/react_pconnect.jsx');
     config.resolve.alias['@pega/react-sdk-components/lib/components/designSystemExtension/DetailsFields'] = path.resolve(__dirname, '../__mocks__/DetailsFields.js');
+    config.module.rules.push({
+      test: /\.(d.ts)$/,    
+      loader: 'null-loader',
+    });
+    config.module.rules.push({
+      test: /\.(map)$/,    
+      loader: 'null-loader',
+    });
     return config;
   },
-};
+}; 

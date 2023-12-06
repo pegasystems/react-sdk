072bcb43e31eb9c6c23e36a269611b5c89606c5d
serav <Vinod.Seraphin@pega.com>
2023-12-05 21:01:40 -0500

    Use new @pega/auth package for authManager and configAccess code

--- src/samples/AuthPage/index.jsx
+++ src/samples/AuthPage/index.jsx
@@ -1,22 +1,7 @@
 import React from 'react';
-import { getHomeUrl, authIsMainRedirect, authRedirectCallback } from "../../helpers/authManager";
+import { getHomeUrl, authIsMainRedirect, authRedirectCallback } from '@pega/auth/lib/sdk-auth-manager';
+import { authDone } from '@pega/auth/lib/auth-code-redirect';
 
-function getEmbedOriginFromState(state) {
-  let embedOrigin = null;
-  try {
-      // Expect state to contain the embedding page's origin
-      if( state ) {
-          embedOrigin = window.atob(state);
-      }
-  // eslint-disable-next-line no-empty
-  } catch(e) {
-  }
-  if( !embedOrigin ) {
-      // eslint-disable-next-line no-restricted-globals
-      embedOrigin = location.origin;
-  }
-  return embedOrigin;
-}
 
 export default function AuthPage() {
 
@@ -27,45 +12,7 @@ export default function AuthPage() {
       location.href = `${getHomeUrl()}portal`;
     });
   } else {
-    const queryString = window.location.search;
-    const urlParams = new URLSearchParams(queryString);
-    const code = urlParams.get("code");
-    const state = urlParams.get("state");
-    const bTryOpenerLogging = false;
-    // eslint-disable-next-line no-console
-    const fnLog = bTryOpenerLogging ? window.opener.console.log : console.log;
-    let bSuccess = false;
-
-    if (code) {
-        fnLog("Testing");
-        try {
-            window.opener.authCodeCallback(code);
-            bSuccess = true;
-        } catch(e) {
-            fnLog("auth.html: Failed to directly access authCodeCallback.")
-        }
-
-        // Post messages require a targetDomain...trying to pass this via state
-        const embedOrigin = getEmbedOriginFromState(state);
-        if( !bSuccess ) {
-            try {
-                window.opener.postMessage({type:"PegaAuth", code}, embedOrigin);
-                bSuccess = true;
-            } catch(e) {
-                fnLog("auth.html: Failed to directly post message to opener");
-            }
-        }
-
-        if( !bSuccess ) {
-            window.addEventListener("message", (event) => {
-                if( event.data && event.data.type && event.data.type==="PegaAuth" ) {
-                    event.source.postMessage({type:"PegaAuth", code}, embedOrigin);
-                }
-            });
-        }
-
-    }
-
+    authDone();
   }
 
   return (

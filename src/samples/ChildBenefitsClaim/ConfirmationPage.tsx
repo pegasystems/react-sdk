40645e0b4a2778ebeec5eb402fc280793a5fbb9b
Sushree <sushree.mohanty@hmrc.gov.uk>
2024-01-10 13:09:43 +0000

    added language toggle in born abroad ConfirmationPage

--- src/samples/ChildBenefitsClaim/ConfirmationPage.tsx
+++ src/samples/ChildBenefitsClaim/ConfirmationPage.tsx
@@ -74,47 +74,50 @@ const ConfirmationPage = ({ caseId }) => {
     return <ShutterServicePage />;
   } else if (!loading && isBornAbroadOrAdopted) {
     return (
-      <MainWrapper>
-        <div className='govuk-panel govuk-panel--confirmation govuk-!-margin-bottom-7'>
-          <h1 className='govuk-panel__title'> {t('APPLICATION_RECEIVED')}</h1>
-          <div className='govuk-panel__body govuk-!-font-size-27'>
-            {t('POST_YOUR_SUPPORTING_DOCUMENTS')}
+      <>
+        <LanguageToggle />
+        <MainWrapper>
+          <div className='govuk-panel govuk-panel--confirmation govuk-!-margin-bottom-7'>
+            <h1 className='govuk-panel__title'> {t('APPLICATION_RECEIVED')}</h1>
+            <div className='govuk-panel__body govuk-!-font-size-27'>
+              {t('POST_YOUR_SUPPORTING_DOCUMENTS')}
+            </div>
           </div>
-        </div>
-        <h2 className='govuk-heading-m'> {t('WHAT_YOU_NEED_TO_DO_NOW')} </h2>
-        <p className='govuk-body'> {t('THE_INFO_YOU_HAVE_PROVIDED')} </p>
-        <ParsedHTML htmlString={documentList} />
-        <p className='govuk-body'> {t('HMRC_MIGHT_CALL_YOU')} </p>
-        <p className='govuk-body'>
-          {' '}
-          {t('AFTER_YOU_HAVE')}{' '}
-          <a href='' onClick={e => generateReturnSlip(e)}>
-            {t('PRINTED_AND_SIGNED_THE_FORM')} {t('OPENS_IN_NEW_TAB')}
-          </a>
-          , {t('RETURN_THE_FORM_WITH')}{' '}
-        </p>
-        <p className='govuk-body govuk-!-font-weight-bold'>
-          Child Benefit Office (GB)
-          <br />
-          Washington
-          <br />
-          NEWCASTLE UPON TYNE
-          <br />
-          NE88 1ZD
-        </p>
-        <p className='govuk-body'> {t('WE_NORMALLY_RETURN_DOCUMENTS_WITHIN')} </p>
-        <p className='govuk-body'>
-          <a
-            href='https://www.tax.service.gov.uk/feedback/ODXCHB'
-            className='govuk-link'
-            target='_blank'
-            rel='noreferrer'
-          >
-            {t('WHAT_DID_YOU_THINK_OF_THIS_SERVICE')}{' '}
-          </a>
-          {t('TAKES_30_SECONDS')}
-        </p>
-      </MainWrapper>
+          <h2 className='govuk-heading-m'> {t('WHAT_YOU_NEED_TO_DO_NOW')} </h2>
+          <p className='govuk-body'> {t('THE_INFO_YOU_HAVE_PROVIDED')} </p>
+          <ParsedHTML htmlString={documentList} />
+          <p className='govuk-body'> {t('HMRC_MIGHT_CALL_YOU')} </p>
+          <p className='govuk-body'>
+            {' '}
+            {t('AFTER_YOU_HAVE')}{' '}
+            <a href='' onClick={e => generateReturnSlip(e)}>
+              {t('PRINTED_AND_SIGNED_THE_FORM')} {t('OPENS_IN_NEW_TAB')}
+            </a>
+            , {t('RETURN_THE_FORM_WITH')}{' '}
+          </p>
+          <p className='govuk-body govuk-!-font-weight-bold'>
+            Child Benefit Office (GB)
+            <br />
+            Washington
+            <br />
+            NEWCASTLE UPON TYNE
+            <br />
+            NE88 1ZD
+          </p>
+          <p className='govuk-body'> {t('WE_NORMALLY_RETURN_DOCUMENTS_WITHIN')} </p>
+          <p className='govuk-body'>
+            <a
+              href='https://www.tax.service.gov.uk/feedback/ODXCHB'
+              className='govuk-link'
+              target='_blank'
+              rel='noreferrer'
+            >
+              {t('WHAT_DID_YOU_THINK_OF_THIS_SERVICE')}{' '}
+            </a>
+            {t('TAKES_30_SECONDS')}
+          </p>
+        </MainWrapper>
+      </>
     );
   } else {
     return (

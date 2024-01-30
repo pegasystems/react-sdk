4b5d6435c7c76c64781eff42e0bbddd2f334f2c5
gautamkumar-hmrc <151021331+gautamkumar-hmrc@users.noreply.github.com>
2024-01-30 09:59:35 +0000

    Bug:7756 - Fixed DOB error message (#340)
    * fixed DOB error msg
    
    * removed unwanted changes
    
    * review addressed
    
    * fix lint
    
    ---------
    
    Co-authored-by: Gautam (CDIO Chief Digital Product Office) Kumar <U.7867629@columbus.local>

--- src/components/helpers/formatters/DateErrorFormatter.js
+++ src/components/helpers/formatters/DateErrorFormatter.js
@@ -1,6 +1,6 @@
 import i18n from 'i18next';
 
-const _DateErrorFormatter = (message, propertyName) => {
+const _DateErrorFormatter = message => {
   const dateRegExp = /(\d*-\d*-\d*)/;
   const matchedDates = message.match(dateRegExp);
   const originalDate = matchedDates?.length > 0 ? matchedDates[0] : null;
@@ -25,9 +25,7 @@ const _DateErrorFormatter = (message, propertyName) => {
       else missingPartMessage += ` ${i18n.t('A_YEAR')}`;
       targets.push('year');
     }
-    const shortPropertyName = String(propertyName).includes('?')
-      ? i18n.t('DATE_OF_BIRTH')
-      : propertyName;
+    const shortPropertyName = i18n.t('DATE');
 
     if (missingPartMessage.length > 0) {
       return {
@@ -45,9 +43,9 @@ const _DateErrorFormatter = (message, propertyName) => {
 
 export const DateErrorFormatter = (message, propertyName) => {
   if (propertyName === ' ') propertyName = i18n.t('DATE_OF_BIRTH');
-  return _DateErrorFormatter(message, propertyName).message;
+  return _DateErrorFormatter(message).message;
 };
 
 export const DateErrorTargetFields = message => {
-  return _DateErrorFormatter(message, null).targets;
+  return _DateErrorFormatter(message).targets;
 };

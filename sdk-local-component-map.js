d9286db0246eeca94a6525b34fc4ba46d0b45f0c
tumms2021389 <sivaramakrishna.tumma@in.pega.com>
2024-02-06 18:53:31 +0530

    fix: Overrides components local map update

--- sdk-local-component-map.js
+++ sdk-local-component-map.js
@@ -1,12 +1,12 @@
 // Statically load all "local" components that aren't yet in the npm package
 
-/*import end - DO NOT REMOVE*/
+/* import end - DO NOT REMOVE */
 
 // localSdkComponentMap is the JSON object where we'll store the components that are
 // found locally. If not found here, we'll look in the Pega-provided component map
 
 const localSdkComponentMap = {
-  /*map end - DO NOT REMOVE*/
+  /* map end - DO NOT REMOVE */
 };
 
 export default localSdkComponentMap;

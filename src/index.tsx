bd4ab0c5eb8e1c413fcbae269ede1343a183c028
Ian Ovenden <ian.ovenden@gmail.com>
2024-02-16 14:30:10 +0000

    Implementation of GDS alternative design system - framework

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,17 @@
 // from react_root.js
-import React from "react";
-import { render } from "react-dom";
+import React from 'react';
+import { render } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import TopLevelApp from '../src/samples/TopLevelApp';
+import '../assets/css/gdsStyles.scss';
 
-const outletElement = document.getElementById("outlet");
+const outletElement = document.getElementById('outlet');
 
-if(outletElement){
-  render((
+if (outletElement) {
+  render(
     <BrowserRouter>
       <TopLevelApp />
-    </BrowserRouter>
-  ), document.getElementById("outlet"));
+    </BrowserRouter>,
+    document.getElementById('outlet')
+  );
 }

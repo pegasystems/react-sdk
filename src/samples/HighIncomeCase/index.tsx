fe59c6c65c0e4bc1a2ae60dd9cd3b85f117c05c0
peter.maloney1@hmrc.gov.uk <U.7293773@columbus.local>
2024-03-19 15:18:24 +0000

    clear up unused import

--- src/samples/HighIncomeCase/index.tsx
+++ src/samples/HighIncomeCase/index.tsx
@@ -1,4 +1,4 @@
-import React, { FunctionComponent, useState, useEffect, useMemo} from 'react';
+import React, { FunctionComponent, useState, useEffect} from 'react';
 import { useHistory } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
 import TimeoutPopup from '../../components/AppComponents/TimeoutPopup';

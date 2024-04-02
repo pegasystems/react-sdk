bf81cbdfc0d7f9a404ddcd1399fe8527f1a7fed4
peter.maloney1@hmrc.gov.uk <U.7293773@columbus.local>
2024-04-02 15:28:39 +0100

    hicbc-optin landing page shutter logic

--- src/samples/HighIncomeCase/index.tsx
+++ src/samples/HighIncomeCase/index.tsx
@@ -1,17 +1,66 @@
-import React, { FunctionComponent, useState } from 'react';
+import React, { FunctionComponent, useState, useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
 import LandingPage from './LandingPage';
 import ClaimPage from './ClaimPage';
-import { registerServiceName } from '../../components/helpers/setPageTitleHelpers';
+import setPageTitle, { registerServiceName } from '../../components/helpers/setPageTitleHelpers';
+import { getSdkConfig } from '@pega/react-sdk-components/lib/components/helpers/config_access';
+import AppHeader from './reuseables/AppHeader';
+import MainWrapper from '../../components/BaseComponents/MainWrapper';
+import AppFooter from '../../components/AppComponents/AppFooter';
 
 const HighIncomeCase: FunctionComponent<any> = () => {
     const [showLandingPage, setShowLandingPage] = useState<boolean>(!window.location.search.includes('code')); 
+    const [shuttered, setShuttered] = useState(null)
+
     const {t} = useTranslation();   
     registerServiceName(t('HIGH_INCOME_BENEFITS'));
     const landingPageProceedHandler = () => {
         localStorage.setItem('showLandingPage', 'false');
         setShowLandingPage(false);
     }
+
+    useEffect(()=>{
+        getSdkConfig().then((config)=>{
+            config.hicbcOptinConfig?.shutterService ? setShuttered(config.hicbcOptinConfig.shutterService) : setShuttered(false)
+        })
+    }, []);
+
+
+    if(shuttered === null) {
+        return null;
+    } else if(shuttered){  
+        setPageTitle();
+        return (  
+            <>
+                {/* <AppHeader
+                    appname={useTranslation().t('HIGH_INCOME_BENEFITS')}
+                    hasLanguageToggle={false}                   
+                />
+                <div className='govuk-width-container'>                
+                    <MainWrapper>                            
+                        <ShutterServicePage />
+                    </MainWrapper>
+                </div> */}
+                <AppHeader
+                    appname={t('HIGH_INCOME_BENEFITS')}
+                    hasLanguageToggle={false}                   
+                />
+                <div className='govuk-width-container'>                
+                    <MainWrapper showPageNotWorkingLink={false}>                            
+                        <div className="govuk-grid-row">
+                            <div className="govuk-grid-column-two-thirds">
+                            <h1 className="govuk-heading-l">Sorry, the service is unavailable</h1>
+                            <p className="govuk-body">Try again later.</p>
+                            <p className="govuk-body">You can return to <a className="govuk-link" href="https://www.gov.uk/child-benefit">Child Benefit guidance</a></p>          
+                            </div>
+                        </div>
+                    </MainWrapper>
+                </div>
+                <AppFooter />
+            </>
+        )
+    }
+    else {
     return (
         <>            
             {showLandingPage ?
@@ -19,5 +68,6 @@ const HighIncomeCase: FunctionComponent<any> = () => {
             : <ClaimPage/>}
         </>
     )
+    }
 }
 export default HighIncomeCase;

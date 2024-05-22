f7bc9de3615c2619ff57ecfa6d235c42e0267150
Ian Ovenden <ian.ovenden@gmail.com>
2024-05-22 11:01:20 +0100

    HICBC AppContext fix to ensure DX components can be built and published (#498)

--- src/samples/HighIncomeCase/reuseables/AppContext.ts
+++ src/samples/HighIncomeCase/reuseables/AppContext.ts
@@ -1,22 +1,19 @@
-
 import { createContext } from 'react';
 
-
 export interface AppContextValues {
-    appBacklinkProps:{
-        appBacklinkAction?:Function,
-        appBacklinkText?:String
-    },
-    showLanguageToggle?: boolean
+  appBacklinkProps: {
+    appBacklinkAction?: Function | null;
+    appBacklinkText?: String | null;
+  };
+  showLanguageToggle?: boolean;
 }
 
-const AppContext =  createContext<AppContextValues>(
-                                {
-                                    appBacklinkProps:{
-                                        appBacklinkAction: null,
-                                        appBacklinkText: null  
-                                    },
-                                    showLanguageToggle: false
-                                });
+const AppContext = createContext<AppContextValues>({
+  appBacklinkProps: {
+    appBacklinkAction: null,
+    appBacklinkText: null
+  },
+  showLanguageToggle: false
+});
 
-export { AppContext as default }
+export { AppContext as default };

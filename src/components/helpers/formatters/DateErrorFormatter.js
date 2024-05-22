bd815a64c3323964c00148442682ae2b8a011315
gautamkumar-hmrc <151021331+gautamkumar-hmrc@users.noreply.github.com>
2024-05-22 10:59:52 +0100

    BUG-8488: Fixed welsh translation for date errors (#502)
    * Fixed welsh translation for date errors
    
    * removed template string
    
    * updated content

--- src/components/helpers/formatters/DateErrorFormatter.js
+++ src/components/helpers/formatters/DateErrorFormatter.js
@@ -12,30 +12,30 @@ const _DateErrorFormatter = message => {
     // When some 'parts' are missing
     let missingPartMessage = '';
     if (day === '') {
-      missingPartMessage += ` ${i18n.t('A_DAY')}`;
+      missingPartMessage += '_A_DAY';
       targets.push('day');
     }
     if (month === '') {
-      if (missingPartMessage.length > 0) missingPartMessage += i18n.t('AND_A_MONTH');
-      else missingPartMessage += i18n.t('A_MONTH');
+      if (missingPartMessage.length > 0) missingPartMessage += '_AND_A_MONTH';
+      else missingPartMessage += '_A_MONTH';
       targets.push('month');
     }
     if (year === '') {
-      if (missingPartMessage.length > 0) missingPartMessage += ` ${i18n.t('AND_A_YEAR')}`;
-      else missingPartMessage += ` ${i18n.t('A_YEAR')}`;
+      if (missingPartMessage.length > 0) missingPartMessage += '_AND_A_YEAR';
+      else missingPartMessage += '_A_YEAR';
       targets.push('year');
     }
-    const shortPropertyName = i18n.t('DATE');
+    const shortPropertyName = 'DATE';
 
     if (missingPartMessage.length > 0) {
       return {
-        message: `${shortPropertyName} ${i18n.t('MUST_INCLUDE')} ${missingPartMessage}`,
+        message: i18n.t(`${shortPropertyName}_MUST_INCLUDE${missingPartMessage}`),
         targets
       };
     }
 
     if (message.search(i18n.t('IS_NOT_A_VALID_DATE'))) {
-      return { message: `${shortPropertyName} ${i18n.t('MUST_BE_A_REAL_DATE')} `, targets };
+      return { message: i18n.t(`${shortPropertyName}_MUST_BE_A_REAL_DATE`), targets };
     }
   }
   return { message, targets };

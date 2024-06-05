6aca434341dceba2faf0e6e58b3b5d63dbf771f0
Vishal <vishal.sharma@in.pega.com>
2024-06-05 12:19:04 +0530

    initial changes

--- sdk-local-component-map.js
+++ sdk-local-component-map.js
@@ -1,11 +1,42 @@
 // Statically load all "local" components that aren't yet in the npm package
-
+import WssNavBar from './src/components/override-sdk/template/WssNavBar/';
+import WssQuickCreate from './src/components/override-sdk/designSystemExtension/WssQuickCreate/';
+import Banner from './src/components/override-sdk/designSystemExtension/Banner/';
+import ToDo from './src/components/override-sdk/widget/ToDo/';
+import AppShell from './src/components/override-sdk/template/AppShell/';
+import QuickCreate from './src/components/override-sdk/widget/QuickCreate/';
+import AssignmentCard from './src/components/override-sdk/infra/AssignmentCard/';
+import ActionButtons from './src/components/override-sdk/infra/ActionButtons/';
+import ListView from './src/components/override-sdk/template/ListView/';
+import Confirmation from './src/components/override-sdk/template/Confirmation/';
+import Region from './src/components/override-sdk/widget/Region';
+import TextInput from '@pega/react-sdk-components/lib/components/field/TextInput';
+import View from './src/components/override-sdk/infra/View/';
+import FlowContainer from './src/components/override-sdk/infra/FlowContainer';
+import CaseView from '@pega/react-sdk-components/lib/components/template/CaseView/CaseView';
+import Details from './src/components/override-sdk/template/Details/';
 /* import end - DO NOT REMOVE */
 
 // localSdkComponentMap is the JSON object where we'll store the components that are
 // found locally. If not found here, we'll look in the Pega-provided component map
 
 const localSdkComponentMap = {
+  WssNavBar,
+  WssQuickCreate,
+  Banner,
+  Todo: ToDo,
+  AppShell,
+  FlowContainer,
+  QuickCreate,
+  AssignmentCard,
+  ActionButtons,
+  ListView,
+  Confirmation,
+  Region,
+  Text: TextInput,
+  View,
+  Pega_Extensions_CaseLauncher: QuickCreate,
+  Details: Details
   /* map end - DO NOT REMOVE */
 };
 

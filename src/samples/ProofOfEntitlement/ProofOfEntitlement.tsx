1cc5d93115e60710f3264f1f0c406e4a9fd5eb3d
Akhila <124593991+AkhilaHMRC@users.noreply.github.com>
2024-08-07 14:41:31 +0100

    update POE page title for error scenario (#665)

--- src/samples/ProofOfEntitlement/ProofOfEntitlement.tsx
+++ src/samples/ProofOfEntitlement/ProofOfEntitlement.tsx
@@ -82,6 +82,7 @@ export default function ProofOfEntitlement() {
           })
           .catch(() => {
             setShowProblemWithService(true);
+            setPageTitle();
           });
       });
 

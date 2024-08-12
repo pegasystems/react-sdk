56eecfc5d7fa63ade9f1548b54113d58a948393e
Bob DiFronzo <Robert.DiFronzo@pega.com>
2024-08-12 09:59:54 -0400

    Removes dependency on @pega/cosmos-react packages.

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,27 +1,8 @@
-import { Configuration, PopoverManager, Toaster, ModalManager, WorkTheme } from '@pega/cosmos-react-core';
-
 import { getSdkComponentMap } from '@pega/react-sdk-components/lib/bridge/helpers/sdk_component_map';
 
 import { decorator } from '../__mocks__/react_pconnect';
 
 getSdkComponentMap();
-
-export const decorators = [
-  (Story, context) => {
-    return (
-      <Configuration>
-        <PopoverManager>
-          <Toaster dismissAfter={5000}>
-            <ModalManager>
-              <Story {...context} />
-            </ModalManager>
-          </Toaster>
-        </PopoverManager>
-      </Configuration>
-    );
-  },
-  decorator
-];
 
 export const parameters = {
   backgrounds: {
@@ -29,15 +10,15 @@ export const parameters = {
     values: [
       {
         name: 'App',
-        value: WorkTheme.base.palette['app-background']
+        value: '#e9eef3'
       },
       {
         name: 'Primary',
-        value: WorkTheme.base.palette['primary-background']
+        value: '#ffffff'
       },
       {
         name: 'Secondary',
-        value: WorkTheme.base.palette['secondary-background']
+        value: '#f5f5f5'
       }
     ]
   },

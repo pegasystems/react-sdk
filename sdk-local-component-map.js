2b722c0d861232c68a2dfc5e0eb2d5ba17d99d62
Siva Rama Krishna <sivaramakrishna.tumma@in.pega.com>
2025-09-19 17:40:57 +0530

    Initial changes for hackathon

--- sdk-local-component-map.js
+++ sdk-local-component-map.js
@@ -1,5 +1,7 @@
 // Statically load all "local" components that aren't yet in the npm package
 
+import TextInput from './src/components/override-sdk/field/TextInput/TextInput';
+
 /* import end - DO NOT REMOVE */
 
 // localSdkComponentMap is the JSON object where we'll store the components that are
@@ -7,6 +9,7 @@
 
 const localSdkComponentMap = {
   /* map end - DO NOT REMOVE */
+  TextInput
 };
 
 export default localSdkComponentMap;

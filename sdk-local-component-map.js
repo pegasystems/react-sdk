c1a1311f2f408337a4a3f7518219857585d8fe87
Sharma <vishal.sharma@in.pega.com>
2026-01-16 16:52:56 +0530

    my changes

--- sdk-local-component-map.js
+++ sdk-local-component-map.js
@@ -1,11 +1,13 @@
 // Statically load all "local" components that aren't yet in the npm package
 
+import BannerPage from './src/components/override-sdk/template/BannerPage/';
 /* import end - DO NOT REMOVE */
 
 // localSdkComponentMap is the JSON object where we'll store the components that are
 // found locally. If not found here, we'll look in the Pega-provided component map
 
 const localSdkComponentMap = {
+  BannerPage: BannerPage
   /* map end - DO NOT REMOVE */
 };
 

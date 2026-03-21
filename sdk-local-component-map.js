8ed380beb17bd922a78fe17352923b03fa26374f
Siva Rama Krishna <sivaramakrishna.tumma@in.pega.com>
2026-03-21 20:23:37 +0530

    refactor: deleted unused field components overrides

--- sdk-local-component-map.js
+++ sdk-local-component-map.js
@@ -8,28 +8,17 @@ import AutoComplete from './src/components/override-sdk/field/AutoComplete/';
 import DateTime from './src/components/override-sdk/field/DateTime/';
 // eslint-disable-next-line no-redeclare
 import Date from './src/components/override-sdk/field/Date/';
-import CancelAlert from './src/components/override-sdk/field/CancelAlert/';
 import Currency from './src/components/override-sdk/field/Currency/';
 import Decimal from './src/components/override-sdk/field/Decimal/';
 import Email from './src/components/override-sdk/field/Email/';
-import Group from './src/components/override-sdk/field/Group/';
 import Integer from './src/components/override-sdk/field/Integer/';
 // eslint-disable-next-line no-redeclare
-import Location from './src/components/override-sdk/field/Location/';
-import Multiselect from './src/components/override-sdk/field/Multiselect/';
-import ObjectReference from './src/components/override-sdk/field/ObjectReference/';
 import Percentage from './src/components/override-sdk/field/Percentage/';
-import Phone from './src/components/override-sdk/field/Phone/';
 import RadioButtons from './src/components/override-sdk/field/RadioButtons/';
 import RichText from './src/components/override-sdk/field/RichText/';
-import ScalarList from './src/components/override-sdk/field/ScalarList/';
-import SemanticLink from './src/components/override-sdk/field/SemanticLink/';
-import SelectableCard from './src/components/override-sdk/field/SelectableCard/';
 import TextArea from './src/components/override-sdk/field/TextArea/';
-import TextContent from './src/components/override-sdk/field/TextContent/';
 import Time from './src/components/override-sdk/field/Time/';
 import Url from './src/components/override-sdk/field/URL/';
-import UserReference from './src/components/override-sdk/field/UserReference/';
 import ActionButtons from './src/components/override-sdk/infra/ActionButtons/';
 import FieldValueList from './src/components/override-sdk/designSystemExtension/FieldValueList/';
 import MultiStep from './src/components/override-sdk/infra/MultiStep/';
@@ -47,27 +36,16 @@ const localSdkComponentMap = {
   AutoComplete: AutoComplete,
   DateTime: DateTime,
   Date: Date,
-  CancelAlert: CancelAlert,
   Currency: Currency,
   Decimal: Decimal,
   Email: Email,
-  Group: Group,
   Integer: Integer,
-  Location: Location,
-  Multiselect: Multiselect,
-  ObjectReference: ObjectReference,
   Percentage: Percentage,
-  Phone: Phone,
   RadioButtons: RadioButtons,
   RichText: RichText,
-  ScalarList: ScalarList,
-  SemanticLink: SemanticLink,
-  SelectableCard: SelectableCard,
   TextArea: TextArea,
-  TextContent: TextContent,
   Time: Time,
   URL: Url,
-  UserReference: UserReference,
   ActionButtons: ActionButtons,
   FieldValueList: FieldValueList,
   MultiStep: MultiStep,

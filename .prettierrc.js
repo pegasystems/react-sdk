d6f7832b16e8b918b0841b35786aa5a178cc17b7
Sharma <vishalshrm539@gmail.com>
2026-06-18 19:21:59 +0530

    Fixed vulnerabilities and errors on building

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,9 +1,11 @@
 // Prettier config options: https://prettier.io/docs/en/options.html
 // Shared front-end config: https://git.pega.io/projects/FE/repos/configs/browse/packages/prettier-config/index.json
 
-const pegaPrettierConfig = require('@pega/prettier-config');
+const pegaPrettierConfig = require('@pega/prettier-config').default || require('@pega/prettier-config');
 
 module.exports = {
   ...pegaPrettierConfig,
-  printWidth: 150
+  printWidth: 150,
+  trailingComma: 'none',
+  arrowParens: 'avoid'
 };
---
+++ types/mui-theme/index.d.ts
@@ -0,0 +1,6 @@
+import { Theme } from '@mui/material/styles';
+
+declare module '@mui/private-theming/defaultTheme' {
+   
+  interface DefaultTheme extends Theme {}
+}
